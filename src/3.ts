d9ee946e8ef4285111587fc13dda9b9ea0d5d7aa
MarynaLapa <133876007+MarynaLapa@users.noreply.github.com>
2024-01-27 10:42:06 +0200

    add 4 task

--- src/3.ts
+++ src/3.ts
@@ -6,13 +6,25 @@
   Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp. 
   Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
+interface ISpellCaster {
+  castSpell(): void;
+}
+
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(): void;
+} 
 
 // реалізація класу Wizard
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
+    this.name = name;
     if (this.level < 1) {
       throw new Error('Level too low');
     }
+    this.level = level;
   }
 
   introduce(phrase: string): void {
